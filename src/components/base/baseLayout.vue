c6ee9260b643328fdf6de4d46e2d117d2253860d
Chris <oscar.sletvold@gmail.com>
2025-02-14 15:08:15 +0100

    first push

---
+++ src/components/base/baseLayout.vue
@@ -0,0 +1,34 @@
+<template>
+    <ion-page>
+        <ion-header :translucent="true">
+            <ion-toolbar>
+                <ion-title>{{ pageTitle }}</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content :fullscreen="true">
+            <slot />
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+} from "@ionic/vue";
+
+export default {
+    props: ['pageTitle'],
+    components: {
+        IonPage,
+        IonHeader,
+        IonToolbar,
+        IonTitle,
+        IonContent,
+    },
+}
+</script>
